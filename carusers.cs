eae05021749ceeb7e3e5a3688406c8de8b150207
AlAzmine482 <137209260+AlAzmine482@users.noreply.github.com>
2024-05-17 14:30:24 -0700

    Add files via upload

---
+++ carusers.cs
@@ -0,0 +1,19 @@
+﻿namespace Webtest.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   
+    
+        public class Cars : IdentityUser
+        {
+
+
+
+        }
+    
+}
